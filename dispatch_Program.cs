a9b34fecfc2b6ac7fa20b69b1fce619527257b71
ruderod <rod.deluhery@gmail.com>
2021-02-03 10:33:04 -0800

    Add files via upload

---
+++ dispatch_Program.cs
@@ -0,0 +1,64 @@
+﻿using Azure;
+using Azure.Storage.Queues;
+using Azure.Storage.Queues.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace QueuesQuickstartV12
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+Console.WriteLine("Azure Queue Storage client library v12 - .NET quickstart sample\n");
+
+// Retrieve the connection string for use with the application. The storage
+// connection string is stored in an environment variable called
+// AZURE_STORAGE_CONNECTION_STRING on the machine running the application.
+// If the environment variable is created after the application is launched
+// in a console or with Visual Studio, the shell or application needs to be
+// closed and reloaded to take the environment variable into account.
+string connectionString = Environment.GetEnvironmentVariable("AZURE_STORAGE_CONNECTION_STRING");
+
+//get number of people and create queue for each
+Console.WriteLine("How many people in this shift?");
+int shifters=System.Console.ReadLine();
+Console.WriteLine($"Ok creating setup for {shifters} staff.");
+
+// Create a unique name for the queue
+
+string queueName = "dispatch-rd-quickstartqueues-" + Guid.NewGuid().ToString();
+string queueName = "dispatch-rd-quickstartqueues-" + Guid.NewGuid().ToString();
+string queueName = "dispatch-rd-quickstartqueues-" + Guid.NewGuid().ToString();
+
+Console.WriteLine($"Creating queue: {queueName}");
+
+// Instantiate a QueueClient which will be
+// used to create and manipulate the queue
+QueueClient queueClient = new QueueClient(connectionString, queueName);
+
+// Create the queue
+await queueClient.CreateAsync();
+
+Console.WriteLine("\nAdding messages to the queue...");
+
+// Send several messages to the queue
+await queueClient.SendMessageAsync("First message");
+await queueClient.SendMessageAsync("Second message");
+
+// Save the receipt so we can update this message later
+SendReceipt receipt = await queueClient.SendMessageAsync("Third message");
+
+Console.WriteLine("\nPeek at the messages in the queue...");
+
+// Peek at messages in the queue
+PeekedMessage[] peekedMessages = await queueClient.PeekMessagesAsync(maxMessages: 10);
+
+foreach (PeekedMessage peekedMessage in peekedMessages)
+{
+    // Display the message
+    Console.WriteLine($"Message: {peekedMessage.MessageText}");
+}
+        }
+    }
+}
